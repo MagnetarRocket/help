fed5cb95d34a04ab99abc1dac35b631e8e5a5012
MagnetarRocket <CoyoteRed@outlook.com>
2024-11-16 23:49:15 -0600

    Create help.sh

---
+++ help.sh
@@ -0,0 +1,14 @@
+echo "These are the avaible commands on this system:\n"
+
+echo "list files + directoies = ls | print current directory = pwd"
+echo "move = mv [orgin file + orgin directory] [target directory]"
+echo "rename = mv [target file] [new name]"
+echo "create directory = mkdir [names] | create blank file = touch [names]"
+echo "read file without pauses = cat [file names] | with pause = more [file names]"
+echo "copy file = cp [target name] [new name] 
+copy to diffrent directory = cp [target name] [target directory]"
+echo "to delete file = rm [file names] | to delete directory [target names]"
+echo "these can be swaped out as needed"
+
+
+!# can be added in with site spefic commands as needed
